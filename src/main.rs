f9a472507d33cee3147b5292dabcb716871f7e3f
Fredrik <freddahf@outlook.com>
2021-05-31 19:50:24 +0200

    getting aloing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,51 @@
 use reqwest::get;
 use serde_json::Value;
+use std::fmt;
 use std::io;
 
 struct Reference {
-    name : String,
-    authors : String,
-    publisher : String,
-    year: u32,
+    name: String,
+    authors: String,
+    pages: String,
+    title: String,
+    publisher: String,
+    year: String,
     address: String,
-    isbn: u32,
+    isbn: String,
+}
+
+impl Reference {
+    fn new(
+        name: String,
+        authors: String,
+        pages: String,
+        title: String,
+        publisher: String,
+        year: String,
+        address: String,
+        isbn: String,
+    ) -> Reference {
+        Reference {
+            name,
+            authors,
+            pages,
+            title,
+            publisher,
+            year,
+            address,
+            isbn,
+        }
+    }
+}
+
+impl fmt::Display for Reference {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "@book{{{}\nauthor = {{{}}}\ntitle = {{{}}}\npages = {{{}}}\npublisher = {{{}}}\nyear = {{{}}}\naddress = {{{}}}\nisbn = {{{}}}\n}}",
+            self.name, self.authors, self.title, self.pages, self.publisher, self.year, self.address, self.isbn
+        )
+    }
 }
 
 /// Gets the author from openlibrary.org/author...
@@ -33,17 +70,46 @@ fn get_authors_links(authors: &[Value]) -> Vec<String> {
 
 #[tokio::main]
 async fn main() -> Result<(), io::Error> {
-    let res = get_by_isbn("https://openlibrary.org/isbn/", "9781292025407").await;
-    println!("{:?}", res);
-    // println!("{:?}", author);
+    let isbn = "9781292025407";
+    let res = get_by_isbn("https://openlibrary.org/isbn/", isbn).await;
     let authors_links = get_authors_links(res["authors"].as_array().unwrap());
     let mut authors: Vec<String> = Vec::new();
     for author_link in authors_links {
         let res = get_author("https://openlibrary.org", &author_link).await;
         authors.push(res["name"].to_string());
     }
-    let tmp = authors.join(" and ");
-    println!("{}", tmp);
-    print!("@book{{}}");
+
+    let tmp = authors.join(" and ").replace('"', "");
+
+    let title = res["title"].to_string().replace('"', "");
+
+    let publishers: String = res["publishers"]
+        .as_array()
+        .unwrap()
+        .into_iter()
+        .map(|publisher| publisher.to_string())
+        .collect::<Vec<String>>()
+        .join(" and ")
+        .replace('"', "");
+
+    let mut date = res["created"]["value"].to_string();
+    date.remove(0);
+    date.truncate(4);
+
+    let pages = res["number_of_pages"].to_string().replace('"',"");
+
+    // println!("{} {} {} {} {}", tmp, title, publishers, isbn, date);
+
+    let reference = Reference::new(
+        "test".to_string(),
+        tmp,
+        pages,
+        title,
+        publishers,
+        date,
+        "kake".to_string(),
+        isbn.to_string(),
+    );
+    println!("{}", reference);
     Ok(())
 }
